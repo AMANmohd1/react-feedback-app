b586aa953e64668b4b2616961a947c811c6f26f1
AMANmohd1 <mohdaman2001@gmail.com>
2022-02-15 15:12:28 +0530

    Feedback form added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+import {v4 as uuidv4} from 'uuid'
+import {useState} from 'react'
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackData from './data/FeedbackData'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 
+function App(){
 
+    const [feedback, setfeedback] = useState(FeedbackData)
 
+    const deleteFeedback = (id) =>{
+        if(window.confirm('Are you sure to delete ?')){
+            setfeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
 
+    const addFeedback = (newFeedback) =>{
+        newFeedback.id = uuidv4()
+        setfeedback([newFeedback,...feedback])
+    }
 
-function App() {
-  return (
-    <>
-    </>
-  );
+    return (
+        <>
+        <Header />
+        <div className="container">
+        <FeedbackForm handleAdd={addFeedback}/>
+        <FeedbackStats feedback={feedback}/>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        </div>
+        </>
+    )
 }
 
-export default App;
+export default App
